c0e795c2e36d7c525ae2f6c3683e4dc1b2917c1c
sdiksha09 <diksha.sharma1209@gmail.com>
2018-08-22 18:14:54 +0530

    first commit

---
+++ app/src/main/java/com/example/shipra/realmdemo/application/realmApplication.java
@@ -0,0 +1,21 @@
+package com.example.shipra.realmdemo.application;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class realmApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // The default Realm file is "default.realm" in Context.getFilesDir();
+        // we'll change it to "myrealm.realm"
+        Realm.init(this);
+        RealmConfiguration config = new RealmConfiguration.Builder().name("product.db").build();
+        Realm.setDefaultConfiguration(config);
+    }
+}
+
+
